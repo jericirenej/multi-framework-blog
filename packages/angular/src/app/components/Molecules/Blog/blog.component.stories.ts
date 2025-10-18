62bcf7db6b9b79e4f643b082c9e951077aa1f060
jericirenej <63595376+jericirenej@users.noreply.github.com>
2025-10-18 16:08:23 +0200

    bugfix/angular-blog-story: fix summary length, void content repeat.

--- packages/angular/src/app/components/Molecules/Blog/blog.component.stories.ts
+++ packages/angular/src/app/components/Molecules/Blog/blog.component.stories.ts
@@ -1,7 +1,6 @@
+import { SUMMARY_LENGTH_LIMIT } from "@/api/constants";
 import { type Meta, type StoryObj } from "@storybook/angular";
 import { BlogComponent } from "./blog.component";
-import { z } from "zod/v4";
-import { SUMMARY_LENGTH_LIMIT } from "@/api/constants";
 
 const date = new Date("2025-06-01T12:00:00");
 const content =
@@ -15,9 +14,14 @@ const meta = {
     blog: {
       author_id: "johnDoe",
       author_name: "John Doe",
-      content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente veniam ut quam ducimus repellendus. Vitae quas maxime quibusdam atque magnam suscipit eligendi mollitia quisquam? Quia minima reiciendis provident dignissimos pariatur!\nLorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla perspiciatis ab sit? Voluptates suscipit placeat eligendi minus corporis, est reiciendis aliquid iste dolorem quae, a, temporibus maiores unde ut aut?\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, eum sit minima unde ullam, magnam aspernatur perspiciatis, velit voluptatem corporis mollitia. Animi natus nisi culpa rem. Tempore dolores magni cumque!",
-      summary: z.string().max(SUMMARY_LENGTH_LIMIT).parse(content),
+      content,
+      summary: [
+        content
+          .replaceAll(/\n+/g, " ")
+          .trim()
+          .slice(0, SUMMARY_LENGTH_LIMIT - 3),
+        "...",
+      ].join(""),
       created_at: date.getTime(),
       updated_at: date.getTime(),
       id: "blog-id",
